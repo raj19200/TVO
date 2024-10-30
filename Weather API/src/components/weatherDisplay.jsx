deecbce3c0b2e8c027b76b35ed352724293cd1e5
Raj <rajshah0192000@gmail.com>
2024-10-30 05:45:49 -0400

    Implement WeatherDisplay Page

---
+++ Weather API/src/components/weatherDisplay.jsx
@@ -0,0 +1,34 @@
+const WeatherDisplay = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Toronto</p>
+        </div>
+        <div>
+          <h1>22°F</h1>
+        </div>
+        <div>sunny</div>
+      </div>
+
+      <div>
+        <div>
+          <p>22°F</p>
+
+          <p>Feels Like</p>
+        </div>
+        <div>
+          <p>22%</p>
+          <p>Humidity</p>
+        </div>
+        <div>
+          <p>14 MPH</p>
+
+          <p>Wind Speed</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
